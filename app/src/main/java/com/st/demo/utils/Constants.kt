5242ad3887e2d55cdc86e7626c105d7895c45362
Alessio Calogiuri <125383114+AleXCalogiuri@users.noreply.github.com>
2026-03-16 12:00:50 +0100

    Update Constants.kt
    deleted host

--- app/src/main/java/com/st/demo/utils/Constants.kt
+++ app/src/main/java/com/st/demo/utils/Constants.kt
@@ -2,11 +2,11 @@ package com.st.demo.utils
 
 class Constants {
     companion object{
-        const val BASE_URL = "http://192.168.1.148:8080/"
-        //"http://192.168.1.72:8080/"
-        const val BASE_URL_SENSOR_DATA = "http://192.168.1.148:8006/"
+        const val BASE_URL = "http://HOST:8080/"
+        
+        const val BASE_URL_SENSOR_DATA = "http://HOST:8006/"
 
-        const val BASE_URL_ROAD_MANAGEMENT = "http://192.168.1.148:8081/"
+        const val BASE_URL_ROAD_MANAGEMENT = "http://HOST:8081/"
 
     }
 }
